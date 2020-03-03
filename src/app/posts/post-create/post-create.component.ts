6b83ccf076169afb05030a4b80975fc67d36577e
webelf000 <steven.jung86000@gmail.com>
2020-03-03 13:47:42 -0500

    add edit post with images

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -51,11 +51,12 @@ export class PostCreateComponent implements OnInit{
             id: postData._id,
             title: postData.title,
             content: postData.content,
-            imagePath: null
+            imagePath: postData.imagePath
           };
           this.form.setValue({
             title: this.post.title,
-            content: this.post.content
+            content: this.post.content,
+            image: this.post.imagePath
           });
         });
       } else {
@@ -88,7 +89,12 @@ export class PostCreateComponent implements OnInit{
         this.form.value.image
         );
     } else {
-      this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content);
+      this.postsService.updatePost(
+        this.postId,
+        this.form.value.title,
+        this.form.value.content,
+        this.form.value.image
+        );
     }
     this.form.reset();
   }
